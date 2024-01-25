d8846660a0e805d6dec7a574b53ba893dbd48c09
Mahmoud-Elwazeer <elwazeermahmoud48@gmail.com>
2024-01-25 19:13:09 +0200

    Log Parsing

---
+++ 0x01-lockboxes/0x03-log_parsing/0-stats.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""import libraries"""
+import sys
+
+"""initial values"""
+count = 0
+size = 0
+lst = []
+out = {'200': 0, '301': 0, '400': 0, '401': 0, '403': 0, '404': 0, '405': 0, '500': 0}
+
+for line in sys.stdin:
+    try:
+        count += 1
+        if count == 11:
+            print('File size: {}'.format(size))
+            for key, value in out.items():
+                if value != 0:
+                    print('{}: {}'.format(key, value))
+            count = 0
+            size = 0
+            lst = []
+            out = {'200': 0, '301': 0, '400': 0, '401': 0, '403': 0, '404': 0, '405': 0, '500': 0}
+        
+        """get data from stdout and read each line"""
+        lst.append(line)
+        """convert data to list to manage"""
+        data = line.split()
+        
+        """calc size"""
+        size += int(data[-1])
+        str_status = data[-2]
+        out[str_status] += 1
+    except Exception:
+        pass
+
