23cfd4882954cc6cfcce654e692751191d7dbe25
Mahmoud-Elwazeer <elwazeermahmoud48@gmail.com>
2024-01-25 19:48:21 +0200

    add another solution

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,60 +1,39 @@
 #!/usr/bin/python3
-"""import libraries"""
+"""
+Log parsing
+"""
+
 import sys
 
-
-def main():
-    """main function"""
-
-    """initial values"""
-    count = 0
-    size = 0
-    lst = []
-    out = {
-        '200': 0,
-        '301': 0,
-        '400': 0,
-        '401': 0,
-        '403': 0,
-        '404': 0,
-        '405': 0,
-        '500': 0
-        }
-
-    for line in sys.stdin:
-        try:
-            count += 1
-            if count == 11:
-                print('File size: {}'.format(size))
-                for key, value in out.items():
-                    if value != 0:
-                        print('{}: {}'.format(key, value))
-                count = 0
-                size = 0
-                lst = []
-                out = {
-                    '200': 0,
-                    '301': 0,
-                    '400': 0,
-                    '401': 0,
-                    '403': 0,
-                    '404': 0,
-                    '405': 0,
-                    '500': 0
-                    }
-
-            """get data from stdout and read each line"""
-            lst.append(line)
-            """convert data to list to manage"""
-            data = line.split()
-
-            """calc size"""
-            size += int(data[-1])
-            str_status = data[-2]
-            out[str_status] += 1
-        except Exception:
-            pass
-
-
 if __name__ == '__main__':
-    main()
+
+    filesize, count = 0, 0
+    codes = ["200", "301", "400", "401", "403", "404", "405", "500"]
+    stats = {k: 0 for k in codes}
+
+    def print_stats(stats: dict, file_size: int) -> None:
+        print("File size: {:d}".format(filesize))
+        for k, v in sorted(stats.items()):
+            if v:
+                print("{}: {}".format(k, v))
+
+    try:
+        for line in sys.stdin:
+            count += 1
+            data = line.split()
+            try:
+                status_code = data[-2]
+                if status_code in stats:
+                    stats[status_code] += 1
+            except BaseException:
+                pass
+            try:
+                filesize += int(data[-1])
+            except BaseException:
+                pass
+            if count % 10 == 0:
+                print_stats(stats, filesize)
+        print_stats(stats, filesize)
+    except KeyboardInterrupt:
+        print_stats(stats, filesize)
+        raise
