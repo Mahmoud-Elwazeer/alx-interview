8359447adce5614c30dbaeded2fbb14031b0ae5d
Mahmoud-Elwazeer <elwazeermahmoud48@gmail.com>
2024-02-01 04:13:18 +0200

    check negative data

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -5,7 +5,7 @@
 def validUTF8(data):
     """determines if a given data set represents a valid UTF-8 encoding."""
     for i in data:
-        if i >= 256:
+        if i >= 256 or i < -128:
             return False
 
     return True
