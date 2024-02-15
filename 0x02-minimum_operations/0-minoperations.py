83926046a5167a0454512df2b9459b19839b8523
Mahmoud-Elwazeer <elwazeermahmoud48@gmail.com>
2024-02-15 20:20:07 +0200

    #

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -6,5 +6,8 @@ def minOperations(n):
     """calculates the fewest number of operations needed to result"""
     if (n <= 1):
         return 0
-    
 
+    count = 2
+    while True:
+        if (n == count):
+            return count
