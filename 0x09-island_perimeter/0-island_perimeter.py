21eb31f5d552c2a375b48e206296fdaf64c7e117
Mahmoud-Elwazeer <elwazeermahmoud48@gmail.com>
2024-03-08 01:01:49 +0200

    island perimeter

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,15 +1,20 @@
 #!/usr/bin/python3
 """Island Perimeter"""
 
-
 def island_perimeter(grid):
     """returns the perimeter of the island described in grid"""
-    lst = []
+    size_y = len(grid)
+    size_x = len(grid[0])
+    perimeter = 0
 
-    for i in grid:
-        lst.append(sum(i))
+    for i in range(size_y):
+        for j in range(size_x):
+            if grid[i][j] == 1:
+                perimeter += 4
+                if grid[i - 1][j] == 1:
+                    perimeter -= 2
+                if grid[i][j - 1] == 1:
+                    perimeter -= 2
 
-    len_x = max(lst)
-    len_y = sum(lst) - max(lst) + 1
+    return (perimeter)
 
-    return ((len_x + len_y) * 2)
